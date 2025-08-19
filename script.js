48718dccb7b2eea2d2bd364af72ff7589a6f0449
ashiqmulki <ashiqmulki11@gmail.com>
2025-08-19 18:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// Get display element
-const display = document.getElementById("display");
 
-// Function to append value
-function appendValue(value) {
-    display.value += value;
-}
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-// Function to clear display
-function clearDisplay() {
-    display.value = "";
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-// Function to calculate result
-function calculateResult() {
-    try {
-        display.value = eval(display.value); // ⚡ evaluates math expression
-    } catch (error) {
-        display.value = "Error";
-    }
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
